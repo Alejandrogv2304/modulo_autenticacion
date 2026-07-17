89f9faa4a2d188f548663956b8a1ad4a9969aac9
AlejandroGv2304 <alejandrogv.1011@gmail.com>
2026-07-17 18:28:03 -0500

    Feat:Agregando el esquema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      //Esto nos permite que la configuracion y variables de entorno sean globales y no tener que hacer esto en cada modulo
+      isGlobal: true,
+      //Nos permite interpolar variables de entorno
+      expandVariables: true,
+    })
+  ],
 
 })
 export class AppModule {}
